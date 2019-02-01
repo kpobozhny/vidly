60ea095968129899d3f64165f4c1268fb0b2b91a
Kostiantyn Pobozhni <k.pobozhny@gmail.com>
2019-02-01 19:01:12 +0200

    start testing

--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,10 @@
 const winston = require('winston');
 const mongoose = require('mongoose');
+const config = require('config');
+
 
 module.exports = function() {
-    mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true })
-        .then(() => winston.info('Connected to MongoDB...'));
+    const db = config.get('db');
+    mongoose.connect(db, { useNewUrlParser: true })
+        .then(() => winston.info(`Connected to ${db}...`));
 }
