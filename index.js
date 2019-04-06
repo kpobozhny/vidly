1bac67e25eed6f56fd0e179e76a9f522ab58ad0e
Kostiantyn Pobozhni <k.pobozhny@gmail.com>
2019-04-07 00:12:48 +0300

    Changed port. Added requiresAuth option

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('./startup/config')();
 require('./startup/validation')();
 
 // PORT
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3090;
 const server = app.listen(port, () => {winston.info(`Listening on port ${port}...`)});
 
 module.exports = server;
